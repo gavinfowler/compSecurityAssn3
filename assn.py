7830fb09f53adf0845742d6799e103ea1a7d5437
Gavin Fowler <gavins.username@gmail.com>
2019-10-13 11:46:31 -0600

    inital commit

---
+++ assn.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+def main():
+    """ Main entry point of the app """
+    print("hello world")
+
+
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
