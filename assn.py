79ff092627c0d7ccee4d6575cf9ac8ab3b39da37
Gavin Fowler <gavins.username@gmail.com>
2019-10-15 14:13:17 -0600

    Added splitting

--- assn.py
+++ assn.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from textwrap import wrap
 
 def hash(plainString):
     hashed = plainString
@@ -9,6 +10,17 @@ def main():
     """ Main entry point of the app """
     print("hello world")
     inputStr = input("Enter string to be hashed: ")
+    n = len(inputStr)//4
+    a = inputStr[0:n]
+    b = inputStr[n:n*2]
+    c = inputStr[n*2:n*3]
+    d = inputStr[n*3:len(inputStr)]
+    print(a)
+    print(b)
+    print(c)
+    print(d)
+    if len(a) == 0 or len(b) == 0 or len(c) == 0 or len(d) == 0:
+        raise Exception('Error string not long enough')
     print(f'Hashed string: {hash(inputStr)}')
 
 
