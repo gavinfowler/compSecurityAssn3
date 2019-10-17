8426a1bd8d5954945021e6b8a1722e1ea54d79a0
Gavin Fowler <gavins.username@gmail.com>
2019-10-16 21:26:24 -0600

    Add help for int to bin

--- assn.py
+++ assn.py
@@ -1,47 +1,68 @@
 #!/usr/bin/env python3
 import binascii
 
-def hash(plainString):
-    hashed = plainString
-    # do hashing stuff here
-    return hashed
+
+def hash(a, b, c, d):
+    a = text_to_bits(a)
+    b = text_to_bits(b)
+    c = text_to_bits(c)
+    d = text_to_bits(d)
+    return binaryFinalResult(a, b, c, d)
+
 
 # from https://stackoverflow.com/a/7397689
-def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
-    bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
+def text_to_bits(text, encoding="utf-8", errors="surrogatepass"):
+    bits = bin(int.from_bytes(text.encode(encoding, errors), "big"))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-def text_from_bits(bits, encoding='utf-8', errors='surrogatepass'):
+
+def text_from_bits(bits, encoding="utf-8", errors="surrogatepass"):
     n = int(bits, 2)
-    return n.to_bytes((n.bit_length() + 7) // 8, 'big').decode(encoding, errors) or '\0'
+    return n.to_bytes((n.bit_length() + 7) // 8, "big").decode(encoding, errors) or "\0"
 
-def binaryToLetters(binA,binB,binC,binD):
-    a = binascii.b2a_uu(binA)
-    print(a)
-    return {'a':a,'b':b,'b':b,'b':b}
 
-def lettersToResult(a,b,c,d):
+def binaryFinalResult(a, b, c, d):
     result = a + b + c + d
     return result
 
+
+def strToNum(string):
+    if len(string) <= 0:
+        return ""
+    temp = ""
+    for char in string:
+        temp += str(ord(char))
+    return temp
+
+
 def main():
     """ Main entry point of the app """
-    print("hello world")
     inputStr = input("Enter string to be hashed: ")
-    n = len(inputStr)//4
+    n = len(inputStr) // 4
     a = inputStr[0:n]
-    b = inputStr[n:n*2]
-    c = inputStr[n*2:n*3]
-    d = inputStr[n*3:len(inputStr)]
-    print(a)
-    print(b)
-    print(c)
-    print(d)
-    if len(a) == 0 or len(b) == 0 or len(c) == 0 or len(d) == 0:
-        raise Exception('Error string not long enough')
-    print(f'Hashed string: {hash(inputStr)}')
+    numA = strToNum(a)
+    b = inputStr[n : n * 2]
+    numB = strToNum(b)
+    c = inputStr[n * 2 : n * 3]
+    numC = strToNum(c)
+    d = inputStr[n * 3 : len(inputStr)]
+    numD = strToNum(d)
+    print(numA)
+    print(numB)
+    print(numC)
+    print(numD)
 
-    print(text_from_bits(text_to_bits('hello')) == 'hello')
+    print(format(12, "b"))
+    print(format(25, "b"))
+
+    print(format(int("1100", 2) + int("11001", 2), "b"))
+    print(format(int(format(12, "b"), 2) + int(format(25, "b"), 2), "b"))
+
+    print(int(format(int(format(12, "b"), 2) + int(format(25, "b"), 2), "b"), 2))
+
+    if len(a) == 0 or len(b) == 0 or len(c) == 0 or len(d) == 0:
+        raise Exception("Error string not long enough, use at least 4 characters")
+    print(f"Hashed string: {hash(a,b,c,d)}")
 
 
 if __name__ == "__main__":
